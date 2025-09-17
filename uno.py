7ae32a80b9c4a6b7cc2f6a2977e6d0e0f3d8ca54
Manu2480 <manuvallejot@gmail.com>
2025-09-17 17:17:45 -0500

    suma

--- uno.py
+++ uno.py
@@ -1,11 +1,12 @@
 nombre = "Manuela"
-nombre = "cleopatra"
+nombre2 = "cleopatra"
 apellido = "reina egipcia"
 edad = 23
 
 print("Hola jero")
 print("hola manu")
 
-print("I am " + nombre + apellido)
-print("I am " +nombre)
+print("I am " + nombre2 + apellido)
 print("mi edad es " + edad )
+
+print("las personas en git son: " + nombre + nombre2)
